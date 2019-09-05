9a7a3d3da9e4f300913282053df77a68c4f84459
Gilles Caulier <caulier.gilles@gmail.com>
2019-09-05 16:50:35 +0200

    show list of items selected from current album

--- generic/helloworldgenericplugin.cpp
+++ generic/helloworldgenericplugin.cpp
@@ -26,6 +26,13 @@
 
 #include <QApplication>
 #include <QMessageBox>
+#include <QList>
+#include <QUrl>
+
+// digiKam includes
+
+#include <dinfointerface.h>
+#include <dmessagebox.h>
 
 namespace DigikamGenericHelloWorldPlugin
 {
@@ -89,9 +96,32 @@ void HelloWorldPlugin::setup(QObject* const parent)
 
 void HelloWorldPlugin::slotHelloWorld()
 {
-    QMessageBox::information(qApp->activeWindow(),
-                             qApp->applicationName(),
-                             QString::fromUtf8("Hello World"));
+    DInfoInterface* const iface = infoIface(sender());
+    QList<QUrl> images          = iface->currentSelectedItems();
+    QString caption             = QString::fromUtf8("List of current selected items (%1):").arg(images.count());
+
+    if (images.isEmpty())
+    {
+        images  = iface->currentAlbumItems();
+        caption = QString::fromUtf8("List of all items (%1):").arg(images.count());
+
+    }
+
+    if (!images.isEmpty())
+    {
+        QStringList items;
+
+        foreach (const QUrl& url, images)
+        {
+            items << url.url();
+        }
+
+        DMessageBox::showInformationList(QMessageBox::Information,
+                                         qApp->activeWindow(),
+                                         QString::fromUtf8("Hello World"),
+                                         caption,
+                                         items);
+    }
 }
 
 } // namespace DigikamGenericHelloWorldPlugin
