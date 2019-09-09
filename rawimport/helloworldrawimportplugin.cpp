209b39edc2edce77fe7497c64ea487d85c1ddd69
Gilles Caulier <caulier.gilles@gmail.com>
2019-09-09 18:41:38 +0200

    first version of HelloWorld Dplugin Raw Import based on dcraw CLI engine

---
+++ rawimport/helloworldrawimportplugin.cpp
@@ -0,0 +1,92 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * https://www.digikam.org
+ *
+ * Date        : 2019-09-09
+ * Description : Hello World demo image editor plugin.
+ *
+ * Copyright (C) 2019 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#include "helloworldrawimportplugin.h"
+
+// Qt includes
+
+#include <QApplication>
+#include <QProcess>
+#include <QFileInfo>
+
+namespace DigikamRawImportHelloWorldPlugin
+{
+
+HelloWorldRawImportPlugin::HelloWorldRawImportPlugin(QObject* const parent)
+    : DPluginRawImport(parent)
+{
+}
+
+HelloWorldRawImportPlugin::~HelloWorldRawImportPlugin()
+{
+}
+
+QString HelloWorldRawImportPlugin::name() const
+{
+    return QString::fromUtf8("Hello World");
+}
+
+QString HelloWorldRawImportPlugin::iid() const
+{
+    return QLatin1String(DPLUGIN_IID);
+}
+
+QIcon HelloWorldRawImportPlugin::icon() const
+{
+    return QIcon::fromTheme(QLatin1String("digikam"));
+}
+
+QString HelloWorldRawImportPlugin::description() const
+{
+    return QString::fromUtf8("A demo Hello World plugin");
+}
+
+QString HelloWorldRawImportPlugin::details() const
+{
+    return QString::fromUtf8("<p>This Raw Import plugin is a simple demo.</p>");
+}
+
+QList<DPluginAuthor> HelloWorldRawImportPlugin::authors() const
+{
+    return QList<DPluginAuthor>()
+            << DPluginAuthor(QString::fromUtf8("Gilles Caulier"),
+                             QString::fromUtf8("caulier dot gilles at gmail dot com"),
+                             QString::fromUtf8("(C) 2019"))
+            ;
+}
+
+void HelloWorldRawImportPlugin::setup(QObject* const parent)
+{
+    // TODO Check dcraw availability.
+}
+
+bool HelloWorldRawImportPlugin::run(const QString& filePath, const DRawDecoding& def)
+{
+    QFileInfo fi(filePath);
+    QProcess::execute(QLatin1String("dcraw"), QStringList() << QLatin1String("-v")
+                                                            << QLatin1String("-4")
+                                                            << QLatin1String("-T")
+                                                            << filePath);
+    emit signalDecodedImage(fi.path() + QLatin1Char('/') + fi.completeBaseName() + QLatin1String(".tif"));
+}
+
+} // namespace DigikamRawImportHelloWorldPlugin
---
+++ rawimport/helloworldrawimportplugin.h
@@ -0,0 +1,62 @@
+/* ============================================================
+ *
+ * This file is a part of digiKam project
+ * https://www.digikam.org
+ *
+ * Date        : 2019-09-09
+ * Description : Hello World demo raw import plugin.
+ *
+ * Copyright (C) 2019 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * ============================================================ */
+
+#ifndef DIGIKAM_HELLO_WORLD_RAW_IMPORT_PLUGIN_H
+#define DIGIKAM_HELLO_WORLD_RAW_IMPORT_PLUGIN_H
+
+// Local includes
+
+#include "dpluginrawimport.h"
+
+#define DPLUGIN_IID "org.kde.digikam.plugin.rawimport.HelloWorld"
+
+using namespace Digikam;
+
+namespace DigikamRawImportHelloWorldPlugin
+{
+
+class HelloWorldRawImportPlugin : public DPluginRawImport
+{
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID DPLUGIN_IID)
+    Q_INTERFACES(Digikam::DPluginRawImport)
+
+public:
+
+    explicit HelloWorldRawImportPlugin(QObject* const parent = nullptr);
+    ~HelloWorldRawImportPlugin();
+
+    QString name()                 const override;
+    QString iid()                  const override;
+    QIcon   icon()                 const override;
+    QString details()              const override;
+    QString description()          const override;
+    QList<DPluginAuthor> authors() const override;
+
+    void setup(QObject* const) override;
+
+    bool run(const QString& filePath, const DRawDecoding& def);
+};
+
+} // namespace DigikamRawImportHelloWorldPlugin
+
+#endif // DIGIKAM_HELLO_WORLD_RAW_IMPORT_PLUGIN_H
