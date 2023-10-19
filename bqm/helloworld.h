c38f40b12eac069cee3ec1a107b64407e843a033
Gilles Caulier <caulier.gilles@gmail.com>
2023-10-19 07:50:20 +0200

    update

--- bqm/helloworld.h
+++ bqm/helloworld.h
@@ -6,7 +6,7 @@
  * Date        : 2019-09-01
  * Description : Hello World batch tool.
  *
- * Copyright (C) 2019-2020 by Gilles Caulier <caulier dot gilles at gmail dot com>
+ * Copyright (C) 2019-2023 by Gilles Caulier <caulier dot gilles at gmail dot com>
  *
  * This program is free software; you can redistribute it
  * and/or modify it under the terms of the GNU General
